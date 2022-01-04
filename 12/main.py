396341d58d520fac7a74fc9cd45339515966a80e
Sebastjan Mevlja <38669826+mevljas@users.noreply.github.com>
2022-01-04 21:18:48 +0100

    add solution for the Day 12: Passage Pathing part 2

--- 12/main.py
+++ 12/main.py
@@ -2,6 +2,24 @@ from collections import defaultdict
 from copy import deepcopy
 
 
+def can_visit_small_cave(path, node):
+    """
+    Determined if dfs should visit a specific small cave
+    :param path: list of nodes on a path
+    :param node: current dfs node
+    :return: Bool value
+    """
+    if node not in path:
+        return True
+
+    filtered_list = list(filter(lambda n: n.islower() and n not in {'start', 'end'}, path))
+
+    if len(filtered_list) == len(set(filtered_list)):
+        return True
+
+    return False
+
+
 class Graph:
     paths = []
 
@@ -70,6 +88,56 @@ class Graph:
         self.dfs_util(v, visited, [])
         return self.paths
 
+    def dfs_util2(self, v, visited, path):
+        """
+        A function used by DFS
+        :param v: current node
+        :param visited: set of visited nodes
+        :param path: list of nodes on a path
+        :return: None
+        """
+        # Deep copy variables
+        path = deepcopy(path)
+        visited = deepcopy(visited)
+
+        # Mark the current node as visited
+        # and print it
+        # visited.add(v)
+        path.append(v)
+        # print(v, end=' ')
+
+        if v == 'end':
+            self.paths.append(path)
+            return
+
+        if v.islower() and v not in {'start', 'end'}:
+            visited.add(v)
+
+        # Recur for all the vertices
+        # adjacent to this vertex
+        for neighbour in set(self.graph[v]).difference(visited.intersection({'start', 'end'})):
+            if neighbour.islower() and neighbour not in {'start', 'end'} and not can_visit_small_cave(path, neighbour):
+                continue
+            self.dfs_util2(neighbour, visited, path)
+
+        return
+
+    def dfs2(self, v):
+        """
+        The function to do DFS traversal. It uses
+        recursive DFSUtil()
+        :param v: first node
+        :return: dfs traversal
+        """
+
+        # Create a set to store visited vertices
+        visited = {'start'}
+
+        # Call the recursive helper function
+        # to find DFS traversal
+        self.dfs_util2(v, visited, [])
+        return self.paths
+
 
 def read_file(filename):
     """
@@ -89,5 +157,6 @@ def read_file(filename):
 
 if __name__ == '__main__':
     graph = read_file('input.txt')
-    dfs = graph.dfs('start')
+    # dfs = graph.dfs('start')
+    dfs = graph.dfs2('start')
     print(len(dfs))
